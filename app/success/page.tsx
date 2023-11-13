7d1514ce186d8cd992736bcac315741267b949c8
Tammitha <Infodesk@torquebdc.com>
2023-11-13 08:06:44 -0800

    font color change

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -4,7 +4,7 @@ export default function Success() {
   return (
     <div className='min-h-screen  flex flex-col items-center justify-center'>
       <div className='bg-white p-8 rounded-md shadow-md'>
-        <h1 className='text-2xl font-semibold text-green-600'>Success!</h1>
+        <h1 className='text-2xl font-semibold text-yellow-500'>Success!</h1>
         <p className='mt-4 text-lg'>
           Your form has been successfully submitted.
         </p>
